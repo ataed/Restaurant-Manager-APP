40348475502e1f18bfae528c7eccd101e2f271ba
Atae Edrissi <ataeedrissi@gmail.com>
2020-09-12 23:27:30 +0100

    Added Menu Controller & all ressource for CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::get('/management',function(){
 });
 
 Route::resource('management/category','Management\CategoryController');
+
+Route::resource('management/menu','Management\MenuController');
