f7c99561aa89d1a43894dbc8c1d411b9104876ef
Atae Edrissi <ataeedrissi@gmail.com>
2020-09-12 04:37:58 +0100

    Side bar

---
+++ resources/views/management/inc/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="col-md-4">
+    <div class="list-group">
+
+        <a href="/management/category" class="list-group-item list-group-item-action">
+            <i class="fas fa-bars"></i>
+            Category
+        </a>
+
+
+        <a href="#" class="list-group-item list-group-item-action">
+            <i class="fas fa-bars"></i>
+            Menu
+        </a>
+
+        <a href="#" class="list-group-item list-group-item-action">
+            <i class="fas fa-chair"></i>
+            Table
+        </a>
+
+        <a href="#" class="list-group-item list-group-item-action">
+            <i class="fas fa-users-cog"></i>
+            User
+        </a>
+
+    </div>
