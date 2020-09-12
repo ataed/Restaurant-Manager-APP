6331e60bff9d88e8e73187b0a6562e1ddeb816f9
Atae Edrissi <ataeedrissi@gmail.com>
2020-09-12 04:39:00 +0100

    Mangement view

---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+    <div class="row">
+        @include('management.inc.sidebar')
+    </div>
+    <div class="col-md-8">
+
+    </div>
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+@endsection
