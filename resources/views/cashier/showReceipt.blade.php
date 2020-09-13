d84af6111d29d15fdb81d2db89528cfcce07ce0f
Atae Edrissi <ataeedrissi@gmail.com>
2020-09-13 21:42:49 +0100

    style the receipt

--- resources/views/cashier/showReceipt.blade.php
+++ resources/views/cashier/showReceipt.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restuarnt App - Receipt - SaleID : {{$sale->id}}</title>
-
+    <link type="text/css" rel="stylesheet" href="{{asset('/css/receipt.css')}}" media="all">
+    <link type="text/css" rel="stylesheet" href="{{asset('/css/no-print.css')}}" media="print">
 </head>
 
 <body>
