ecbd12bff6ada8752816c678cba8321b220212b0
Atae Edrissi <ataeedrissi@gmail.com>
2020-09-13 02:22:38 +0100

    Added TableController with resource for CRUD operations

---
+++ app/Http/Controllers/Management/TableController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Management;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Table;
+
+class TableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tables = Table::all();
+        return view('/management/table.table')->with('tables',$tables);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('/management/table.createTable');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:tables|max:255'
+            ]);
+            
+            $table = new Table();
+
+        $table->name = $request->name;
+        $table->save();
+
+        session()->flash('status',$request->name. " table saved successfully!");
+        return redirect('/management/table');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $table = Table::find($id);
+        return view('/management/table.editTable')->with('table',$table);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+       $request->validate([
+          'name'=> 'required|unique:tables|max:255'
+       ]);
+
+       $table = Table::find($id);
+       $table->name = $request->name;
+       $table->save();
+
+       session()->flash('status', "Table updated successfully!");
+        return redirect('/management/table');
+    }
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Table::destroy($id);
+
+        session()->flash('status', "Table deleted successfully!");
+        return redirect('/management/table');
+
+    }
+}
