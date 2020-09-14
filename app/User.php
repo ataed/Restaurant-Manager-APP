8ef8de515741382935a7bbdcc9acb2f491a1c0ae
Atae Edrissi <ataeedrissi@gmail.com>
2020-09-14 03:19:40 +0100

    Added VerifyAdmin Middleware

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+        public function isAdmin(){
+            if($this->role == 'admin'){
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+
 }
